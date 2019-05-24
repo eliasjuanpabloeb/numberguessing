21e7a7aa9e370a2d90a2c25a7b0d2c4d3ac88467
Juan <jpelias_96@hotmail.com>
2019-05-24 12:41:21 -0300

    moved common behavior to setUp method to prevent repetition

--- tests.py
+++ tests.py
@@ -31,58 +31,57 @@ class ValidationTests(TestCase):
 
 
 class HumanGuessingGameTest(TestCase):
+    def setUp(self):
+        self.game = HumanGuessingGame()
+
     def test_get_secret_value(self):
-        game = HumanGuessingGame()
-        secret_number = game.get_secret_value()
+        secret_number = self.game.get_secret_value()
         validate(secret_number)
 
     def test_get_guess(self):
         INPUT_VALUE = '123'
-        game = HumanGuessingGame()
         guess = None
         with mock.patch('builtins.input', return_value=INPUT_VALUE):
-            guess = game.get_guess()
+            guess = self.game.get_guess()
             self.assertEqual(guess, INPUT_VALUE)
 
     def test_check_guess(self):
-        game = HumanGuessingGame()
-
         # Guesses perfectly
-        result = game.check_guess(game.secret_value)
+        result = self.game.check_guess(self.game.secret_value)
         self.assertEqual(result, {'good': 4, 'regular': 0})
 
         # 1 good and 1 regular
-        game.secret_value = '1234'
-        result = game.check_guess('1526')
+        self.game.secret_value = '1234'
+        result = self.game.check_guess('1526')
         self.assertEqual(result, {'good': 1, 'regular': 1})
 
         # All wrong
-        game.secret_value = '1234'
-        result = game.check_guess('4321')
+        self.game.secret_value = '1234'
+        result = self.game.check_guess('4321')
         self.assertEqual(result, {'good': 0, 'regular': 4})
 
     @mock.patch('builtins.print')
     @mock.patch('builtins.input', return_value='1234')
     def test_play(self, mocked_input, mocked_print):
-        game = HumanGuessingGame()
-        game.secret_value = '1234'
-        game.play()
+        self.game.secret_value = '1234'
+        self.game.play()
         mocked_print.assert_any_call('Good: 4\nRegular: 0\n')
         mocked_print.assert_any_call('You win! Game over')
 
 
 class CPUGuessingGameTest(TestCase):
+    def setUp(self):
+        self.game = CPUGuessingGame()
+
     def test_get_guess(self):
-        game = CPUGuessingGame()
-        guess = game.get_guess()
+        guess = self.game.get_guess()
         validate(guess)
 
     @mock.patch('builtins.print')
     @mock.patch('builtins.input', return_value='1,2')
     def test_check_guess(self, mocked_input, mocked_print):
-        game = CPUGuessingGame()
-        guess = game.get_guess()
-        result = game.check_guess(guess)
+        guess = self.game.get_guess()
+        result = self.game.check_guess(guess)
 
         mocked_print.assert_called_with('My guess is {}'.format(guess))
         mocked_input.assert_called_with('Score: ')
@@ -90,6 +89,7 @@ class CPUGuessingGameTest(TestCase):
         self.assertEqual(result, {'good': 1, 'regular': 2})
 
     def test_play(self):
+        # TODO: use random seed to reproduce the same game 
         pass
 
 
